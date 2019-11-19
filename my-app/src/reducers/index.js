c204e47b5818548c36ca809a9cb06c2d0795a4ce
Miley Wright <dishaliekwright@yahoo.com>
2019-11-18 21:12:27 -0500

    MVP

--- my-app/src/reducers/index.js
+++ my-app/src/reducers/index.js
@@ -1,17 +1,50 @@
 // add a reducer file and build out a reducer with a default return. Build your initial State object and export both the initial State and reducer.
-export const initialState ={
-    item: 'Learn about reducers',
-    completed: false,
-    id: 3389820
-};
-
-export const titleReducer = (state,action) =>{
-    switch(action.type){
-        case 'SET_ITEM':
-            return {...state, item: action.payload};
-        case 'TOGGLE_COMPLETED':
-            return {...state, completed: action.payload};
-        default:
-            return state;
-    }
+export const initialState = {
+    listItem: '',
+    todoList: []
 }
+
+export const reducer=(state, action) =>{
+    switch (action.type) {
+      case 'INPUT':
+          return {
+              ...state, 
+              listItem: action.value
+          }
+      case 'ADD_ITEM':
+          const newID = new Date().getTime()
+          const newItem = {
+            item: action.item,
+            completed: false,
+            id: newID
+          }
+          const newList = [...state.todoList, newItem]
+          return {
+            ...state, 
+            todoList: newList
+          }
+      case 'REMOVE_ITEM':
+          const newStateRemove = state.todoList.filter(ele => ele.id!==action.id)
+          return {
+              ...state,
+              todoList: newStateRemove
+          }
+      case 'COMPLETE_ITEM':
+          const newStateCompleted= state.todoList.map(
+            el =>`${el.id}`===`${action.id}` ? {...el, completed: !el.completed} : el
+          )
+          return {
+            ...state,
+            todoList: newStateCompleted
+          }
+      case 'CLEAR_COMPLETED':
+          const newStateClear=state.todoList.filter(ele=> !ele.completed)
+          return {
+            ...state,
+            todoList: newStateClear
+          }
+
+      default:
+        return state
+    }
+  }
